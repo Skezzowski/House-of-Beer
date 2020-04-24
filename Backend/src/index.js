71c0c323cb83a4f484a441f7cf26ea8a7241ea7a
Balazs Sipos <sipblai@gmail.com>
2020-04-24 18:39:01 +0200

    Initial commit

---
+++ Backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// ezekkel lépteti be a passport sessionbe a usert majd szedi ki onnan
+passport.serializeUser((user, done) => {
+    if(!user) return done("Hiba - nincs user", undefined);
+    console.log("serialize sikeres");
+    return done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    console.log("deserialize meghivva");
+    if(!user) return done("Hiba - nincs user, akit kileptethetnenk", undefined);
+    console.log("deserialize sikeres");
+    return done(null, user);
+});
+
+passport.use('local', new localStrategy((username, password, done) => {
+    const users = require('./users.json').users;
+    if(users[username] && users[username].password === password) {
+        return done(null, {username: username, role: users[username].role});
+    } else {
+        return done("Hibás felhasználónév vagy jelszó", undefined);
+    }
+}));
+
+app.use(expressSession({secret: 'ezegyujabbprfgyakorlatmarnemtudokujsecretetkitalalni'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', require('./routes/login-routes'));
+
+app.listen(3000, () => {
+    console.log('the server is running');
+});
