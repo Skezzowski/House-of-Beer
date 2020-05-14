cb74023b85d4bfdab69cae745c9b791fb6b08317
Terhes Balázs <bterhi13@gmail.com>
2020-05-14 15:55:22 +0200

    Login service basics

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(private loginServie: LoginService) { }
 
 }
