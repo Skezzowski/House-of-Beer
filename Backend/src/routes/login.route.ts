064729a7c0594832b409d2f5f6fa7be1447f89f0
Balazs Sipos <sipblai@gmail.com>
2020-05-24 12:22:03 +0200

    AuthChecker endpoint

--- Backend/src/routes/login.route.ts
+++ Backend/src/routes/login.route.ts
@@ -45,4 +45,12 @@ router.route('/register').post(userNamePasswordChecker, (req, res) => {
 		})
 });
 
+router.route('/authcheck').get((req, res) => {
+	if (req.session?.passport.user) {
+		return res.status(200).json({ isAuthenticated: true });
+	} else {
+		return res.status(200).json({ isAuthenticated: false });
+	}
+});
+
 module.exports = router;
